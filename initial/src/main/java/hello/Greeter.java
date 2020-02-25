5730cf75f9e391905614019232779eb5fc15ee10
root <daniela.becerra@digitalonus.com>
2020-02-25 13:12:36 -0600

    JAcoco implemented and first coverage results are given

--- initial/src/main/java/hello/Greeter.java
+++ initial/src/main/java/hello/Greeter.java
@@ -4,4 +4,8 @@ public class Greeter {
 	public String sayHello() {
 		return "Hello world!";
 	}
+
+        public String sayHelloTo(String name) {
+                return "Hi, " + name + ". Hello World!" ;
+        }	
 }
--- initial/src/test/java/GreeterTest.java
+++ initial/src/test/java/GreeterTest.java
@@ -14,4 +14,9 @@ public class GreeterTest {
     assertThat(greeter.sayHello(), containsString("Hello"));
   }
 
+  @Test
+  public void greeterSaysHelloTo() {
+    assertEquals(greeter.sayHelloTo("papas"), "Hi, papas. Hello World!"); 
+  }
+
 }
